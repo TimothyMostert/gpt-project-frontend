08799b1225ade5348a01751ed0bd7e692892c446
Timothy Mostert <resident@hotmail.co.za>
2023-06-11 15:45:00 +0200

    google gtag

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp, markRaw } from "vue";
 import { createPinia } from 'pinia'
 
-import { createGtm } from '@gtm-support/vue-gtm';
 import VueSocialSharing from 'vue-social-sharing';
 
 import App from './App.vue'
 import router from './router'
-
+import VueGtag from "vue-gtag";
 
 import './assets/main.css'
 
@@ -18,18 +17,14 @@ pinia.use(({ store }) => {
   store.$router = markRaw(router);
 });
 
+app.use(VueGtag, {
+  config: { id: "G-PY0DS4KDBG" }
+}, router);
 
 app.use(pinia);
 
 app.use(router);
 
 app.use(VueSocialSharing);
-
-app.use(
-  createGtm({
-    id: "GTM-MX9KP6H",
-    vueRouter: router
-  })
-)
 
 app.mount("#app");
